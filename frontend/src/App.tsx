03eb4866aabe54047cdf09505dc4d32d15cb2d8d
Rudy Pena <rudy.pena@dtn.com>
2024-08-21 21:17:16 -0500

    feat(frontend): add missing info, guests, top5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Episode } from "./types";
 import Input from "./Input";
 
 import "./App.css";
+import Article from "./Article";
 
 function App() {
   const [data, setData] = useState<Episode[]>([]);
@@ -20,66 +21,27 @@ function App() {
         >
           <Input hasResults={data.length > 0} setData={setData} />
         </div>
-        {JSON.stringify(data)}
+
+        {/* {JSON.stringify(data)} */}
+
         {data.length > 0 && (
-          <div>
+          <div className="md:w-2/4">
             {data.map((item, idx) => (
-              <article
+              <Article
                 key={item._id}
-                className="group relative flex flex-col items-start mb-10"
-              >
-                <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                  <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                  <a href="/articles/introducing-animaginary">
-                    <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                    <span className="relative z-10">
-                      {item.title}
-                      {/* Introducing Animaginary: High performance web animations */}
-                    </span>
-                  </a>
-                </h2>
-                <time
-                  className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
-                  // datetime="2022-09-02"
-                >
-                  <span
-                    className="absolute inset-y-0 left-0 flex items-center"
-                    aria-hidden="true"
-                  >
-                    <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-                  </span>
-                  {item.date}
-                </time>
-                <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                  {item.notes}
-                  {/* When you’re building a website for a company as ambitious as
-                Planetaria, you need to make an impression. I wanted people to
-                visit our website and see animations that looked more realistic
-                than reality itself. */}
-                </p>
-                <div
-                  aria-hidden="true"
-                  className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
-                >
-                  <a title={item.title} href={item.url}>
-                    See full details.
-                  </a>
-                  <svg
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    aria-hidden="true"
-                    className="ml-1 h-4 w-4 stroke-current"
-                  >
-                    <path
-                      d="M6.75 5.75 9.25 8l-2.5 2.25"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
-                </div>
-              </article>
+                _id={item._id}
+                title={item.title}
+                date={item.date}
+                notes={item.notes}
+                url={item.url}
+                episode_no={item.episode_no}
+                guests={item.guests}
+                top_5_comparison_year={item.top_5_comparison_year}
+              />
             ))}
+
+            <button>Next</button>
+            <button>Prev</button>
           </div>
         )}
       </main>
